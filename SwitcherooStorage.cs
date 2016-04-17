b1b1b7ac29d7c230e148a93d4d6e8c70e89d1bd1
Boris Makogonyuk <madrivus@gmail.com>
2016-04-17 11:46:53 +0200

    Plugin project upgraded to .NET 4.5.2. Fixed Wox.Infrastructure incompatibility

--- SwitcherooStorage.cs
+++ SwitcherooStorage.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Newtonsoft.Json;
@@ -9,15 +10,8 @@ namespace Wox.Plugin.Switcheroo
     public class SwitcherooStorage : JsonStrorage<SwitcherooStorage>
     {
         [JsonProperty]
-        public bool OverrideAltTab { get; set; }
-        
-        protected override string ConfigFolder
-        {
-            get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
-        }
-
-        protected override string ConfigName { get; } = "SwitcherooSettings";
-
+        public bool OverrideAltTab { get; set; }        
+        protected override string FileName { get; } = "SwitcherooSettings";
         protected override SwitcherooStorage LoadDefault()
         {
             OverrideAltTab = false;
