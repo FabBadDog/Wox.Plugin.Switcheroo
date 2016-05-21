3972296aaea860e65c462be759a7ef1ff0417257
happlebao <baozhi1992@gmail.com>
2016-05-21 02:49:25 +0100

    fixup
    1. fix https://github.com/Wox-launcher/Wox.Plugin.Switcheroo/issues/5
    2. fix https://github.com/Wox-launcher/Wox/issues/640

--- Plugin.cs
+++ Plugin.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.Caching;
-using System.Threading;
 using System.Windows.Forms;
-using System.Windows.Media.Imaging;
 using ManagedWinapi.Windows;
 using Switcheroo;
 using Switcheroo.Core;
 using Wox.Infrastructure.Hotkey;
 using Wox.Infrastructure.Storage;
-using Wox.Plugin.Features;
 using Control = System.Windows.Controls.Control;
 
 namespace Wox.Plugin.Switcheroo
@@ -36,8 +31,8 @@ namespace Wox.Plugin.Switcheroo
         }
 
         public List<Result> Query(Query query)
-        {       
-            var queryString = query.GetAllRemainingParameter();
+        {
+            var queryString = query.Search;
 
             var windowContext = new WindowFilterContext<AppWindowViewModel>
             {
@@ -45,7 +40,7 @@ namespace Wox.Plugin.Switcheroo
                 ForegroundWindowProcessTitle = new AppWindow(SystemWindow.ForegroundWindow.HWnd).ProcessTitle
             };
 
-            var filterResults = new WindowFilterer().Filter(windowContext, queryString).Select(o=>o.AppWindow.AppWindow).ToList();
+            var filterResults = new WindowFilterer().Filter(windowContext, queryString).Select(o => o.AppWindow.AppWindow).ToList();
 
 
             return filterResults.Select(o =>
@@ -54,7 +49,7 @@ namespace Wox.Plugin.Switcheroo
                 {
                     Title = o.Title,
                     SubTitle = o.ProcessTitle,
-                    IcoPath = IconImageDataUri(o),
+                    IcoPath = o.ExecutablePath,
                     ContextData = o,
                     Action = con =>
                     {
@@ -85,52 +80,20 @@ namespace Wox.Plugin.Switcheroo
 
         private void OnAltTabPressed()
         {
-            
-            //return as soon as possible
-            ThreadPool.QueueUserWorkItem(o =>
-            {
-                context.API.HideApp();
-                context.API.ShowApp();
-                context.API.ChangeQuery(context.CurrentPluginMetadata.ActionKeyword + " ", true);
-            });
+            context.API.ShowApp();
+            context.API.ChangeQuery(context.CurrentPluginMetadata.ActionKeyword + " ", true);
         }
 
-
-        public string IconImageDataUri(AppWindow self)
-        {
-            var key = "IconImageDataUri-" + self.HWnd;
-            var iconImageDataUri = MemoryCache.Default.Get(key) as string;
-            if (iconImageDataUri == null)
-            {
-                try
-                {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        BitmapEncoder encoder = new PngBitmapEncoder();
-                        encoder.Frames.Add(BitmapFrame.Create(new IconToBitmapImageConverter().Convert(self.LargeWindowIcon)));
-                        encoder.Save(memoryStream);
-                        var b64String = Convert.ToBase64String(memoryStream.ToArray());
-                        iconImageDataUri = "data:image/png;base64," + b64String;
-                        MemoryCache.Default.Add(key, iconImageDataUri, DateTimeOffset.Now.AddHours(1));
-                    }
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-            return iconImageDataUri;
-        }
 
         public List<Result> LoadContextMenus(Result selectedResult)
         {
-            AppWindow app = (AppWindow) selectedResult.ContextData;
+            AppWindow app = (AppWindow)selectedResult.ContextData;
             return new List<Result>
             {
                 new Result
                 {
                     Title = "Close " + app.Title,
-                    IcoPath = IconImageDataUri(app),
+                    IcoPath = app.ExecutablePath,
                     Action = e =>
                     {
                         context.API.ChangeQuery(context.CurrentPluginMetadata.ActionKeyword + " ", true);
